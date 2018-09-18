2e7fe0a048363b860c058c71ecf5ace944fc220c
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2018-09-18 18:25:54 +0530

    Q1, Q2 modified

--- A1/q1.c
+++ A1/q1.c
@@ -1,9 +1,23 @@
 #include "omp.h" /* OpenMP compiler directives, APIs are declared here */
 
 void main() {
+    double start_time = omp_get_wtime();
     /* Parallel region begins here */
     #pragma omp parallel
     {
         printf("Hello World\n");
     }
+    double time_parallel =  omp_get_wtime() - start_time;
+
+    printf("Serial\n");
+    start_time = omp_get_wtime();
+    // serial 4 times
+    for (int i = 0; i < 4; i++) {
+        printf("Hello World\n");
+    }
+    double time_serial =  omp_get_wtime() - start_time;
+    
+    // results
+    printf("Parallel time: %lf\n", time_parallel);
+    printf("Serial time: %lf\n", time_serial);
 }
--- A1/q2.c
+++ A1/q2.c
@@ -5,10 +5,24 @@ void printHelloWorld(int threadId) {
 }
 
 void main() {
+    double start_time = omp_get_wtime();
     /* Parallel region begins here */
     #pragma omp parallel
     {
         int id = omp_get_thread_num();
         printHelloWorld(id);
     }
+    double time_parallel =  omp_get_wtime() - start_time;
+
+    printf("Serial\n");
+    start_time = omp_get_wtime();
+    // serial 4 times 
+    for (int i = 0; i < 4; i++) {
+        printHelloWorld(i);
+    }
+    double time_serial =  omp_get_wtime() - start_time;
+
+    // results
+    printf("Parallel time: %lf\n", time_parallel);
+    printf("Serial time: %lf\n", time_serial);
 }
